df1798c919938d7a2a1fd3e9307da3532de93749
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-09 22:35:14 +0000

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+# include "holberton.h"
+/**
+*_isalpha - function to check if c is a letter, lowercase or uppercase
+* @c: is the int that will use for the argument of the function
+* Return: 0
+*/
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
