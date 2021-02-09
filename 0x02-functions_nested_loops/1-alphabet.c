a0b2ef81be854151aca2476b96c2c2f2aaadb55b
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-09 15:49:17 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * print_alphabet to print abc
+ * print_alphabet - function to print abc
  *
- * Return: Always 0.
+ * Return: 0
  */
 void print_alphabet(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
-	_putchar(c);	
+_putchar(c);
 }
 _putchar('\n');
 }
