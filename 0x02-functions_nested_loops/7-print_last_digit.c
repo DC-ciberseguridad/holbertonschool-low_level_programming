834ebde3b64c95d6e5cc61fb33cd7e9249646006
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-10 05:08:33 +0000

    function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,12 @@
 
 int print_last_digit(int c)
 {
-if (c > 0)
-{
-_putchar (c % 10 + '0');
-return (c % 10);
-}
-else
-{
+
+c = c % 10;
+
+if (c < 0)
+
 c = c * -1;
 _putchar (c % 10 + '0');
 return (c % 10);
-}
 }
