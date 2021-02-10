ae304ef6ab7ea26683fc3fb14abc53bcbffc155d
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-10 16:41:14 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *add - Adds two integers and returns the result
+ *n1 , n2@ numbers being added
+ *
+ *return 0
+ */
+
+int add(int n1, int n2)
+{
+	int rest = n1 + n2;
+
+	return (rest);
+}
