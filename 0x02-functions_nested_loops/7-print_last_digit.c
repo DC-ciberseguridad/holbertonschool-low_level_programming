9a9cb241b8e44dffccef107368fff2fd9e80c03e
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-10 04:17:17 +0000

    function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * print_last_digit - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
- * Return: 0
- */
+* print_last_digit - function that computes the absolute value of an integer
+* @c:  is the int that will use for the argument of the function
+* Return: 0
+*/
 
 int print_last_digit(int c)
 {
