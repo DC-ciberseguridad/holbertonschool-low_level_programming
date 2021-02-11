cc2f83d534a0eda3d709c4863b5fd85ab1d43b86
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-11 17:28:20 +0000

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
- * _isupper - function that checks for uppercase character.
- * @c: the int for the paramaters of my function
- * Return: 0
+ * _isupper - checks for uppercase characters.
+ *@c: character being tested
+ * Return: Always 0.
  */
 
 int _isupper(int c)
 {
-
 	if (c >= 65 && c <= 90)
-	{
 		return (1);
-	}
 	else
 		return (0);
-
 }
