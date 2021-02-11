6e0716e023226afaa31b86d14eaeb6fb8784dc23
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-11 18:13:03 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *mul -function that multiplies two integers.
+ *@a: is first intiger variable
+ *@b: is second intiger variable
+ *Return: multiplication of two integers
+ */
+
+
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+
+}
