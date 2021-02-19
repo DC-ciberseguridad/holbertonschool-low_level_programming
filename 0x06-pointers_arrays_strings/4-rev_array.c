5aa6e49af8bd5d27a786b1fcd3d74c87f31ee83f
root <root@ip-172-31-36-209.sa-east-1.compute.internal>
2021-02-19 02:43:52 +0000

    function that reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,20 +11,20 @@
 
 void reverse_array(int *a, int n)
 {
-        int *ar, i, j, c;
-	
-		ar = a;
-		
-        for (i = 1; i < n; i++)
-        {
-			ar++;
-		}
+	int *ar, i, j, c;
 
-        for (c = 0; c < i / 2; c++)
-        {
-                j = a[c];
-                a[c] = *ar;
-                *ar = j;
-                ar--;
-        }
+	ar = a;
+
+	for (i = 1; i < n; i++)
+	{
+		ar++;
+	}
+
+	for (c = 0; c < i / 2; c++)
+	{
+		j = a[c];
+		a[c] = *ar;
+		*ar = j;
+		ar--;
+	}
 }
